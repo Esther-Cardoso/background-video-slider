8427f78953d0dc92c636ad7d7fef76e451c87f0d
Esther Cardoso <esthercardosofernandes@gmail.com>
2023-08-02 20:13:28 -0300

    slide concluido

--- index.js
+++ index.js
@@ -16,3 +16,26 @@ function mudarIcon() {
 }
 
 menuBtn.addEventListener('click', mudarIcon)
+
+// javascript for video slider navigation
+const btns = document.querySelectorAll('.nav-btn')
+const slides = document.querySelectorAll('.video-slide')
+
+var sliderNav = function(manual) {
+  btns.forEach((btn) => {
+    btn.classList.remove('active')
+  })
+
+  slides.forEach((slide) => {
+    slide.classList.remove('active')
+  })
+
+  btns[manual].classList.add('active')
+  slides[manual].classList.add('active')
+}
+
+btns.forEach((btn, i) => {
+  btn.addEventListener('click', () => {
+    sliderNav(i)
+  })
+})
